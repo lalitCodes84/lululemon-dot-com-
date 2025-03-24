46fc373e156fd150029bf9e435c1822ee1ca59aa
lalitCodes84 <vickysharma71za@gmail.com>
2025-03-24 14:49:12 +0530

    Initial commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Topbar from './Topbar'
+
+function Navbar() {
+  return (
+    <div>
+       <Topbar/>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/components/Topbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Topbar() {
+  return (
+    <div>
+        <p className='text-center font-'>Get 15% off regular-priced items. <a href="/signup" className='underline'>Sign-up</a></p>
+    </div>
+  )
+}
+
+export default Topbar
