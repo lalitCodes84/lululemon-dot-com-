de837ab480beaf0909c998237738a83901e663e4
lalitCodes84 <vickysharma71za@gmail.com>
2025-03-28 21:36:02 +0530

    Corrected Navbar Navlinks

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Logo from "./Logo.jsx";
 import Icons from "./Icons.jsx";
-import Navlinks from "../components/Navlinks.jsx";
-import SearchBar from "../components/SearchBar.jsx";
+import NavLinks from "./NavLinks.jsx";
+import SearchBar from "./SearchBar.jsx"
+
 
 function Navbar() {
   return (
     <div className="sticky top-0 z-50 flex items-center bg-white w-full h-22">
       <Logo />
-      <Navlinks/>
+      <NavLinks/>
       <SearchBar/>
       <Icons />
     </div>
